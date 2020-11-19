89e2be12a80a25469829a8872790d42872006ef8
Orkun Gürler <orkungurler@gmail.com>
2020-11-19 23:35:30 +0300

    add insertion sort

---
+++ sort/core/insertion.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+void insertion_sort(int *arr, int size, int flag)
+{
+    int temp;
+    int i, j;
+    for (i = 1; i < size; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && compare(temp, arr[j], flag))
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
